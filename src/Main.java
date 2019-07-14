55b6ad6381114c93edf2f61cad4aef3139b1f8b7
Jon Mockbee <mockbee1000@gmail.com>
2019-07-14 16:26:46 -0400

    testing

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,6 @@ public class Main {
 
     public static void main(String[] args) {
         boolean isSum = EqualSumChecker.hasEqualSum(4,5,6);
-        s
+        System.out.println("Is the sum = to numbers? " + isSum);
     }
 }
