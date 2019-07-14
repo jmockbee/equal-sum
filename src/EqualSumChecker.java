9e44ddd75dc2f37f72dd0515bf58409cf951c359
Jon Mockbee <mockbee1000@gmail.com>
2019-07-14 16:21:46 -0400

    just used return vs if else statement

--- src/EqualSumChecker.java
+++ src/EqualSumChecker.java
@@ -1,7 +1,24 @@
 public class EqualSumChecker {
 
-    public static boolean hasEqualSum ( int num1, int num2, int num3){
-        if (num3 == num1 + num2 )
-    }
+    public static boolean hasEqualSum(int num1, int num2, int num3) {
+        return (num3 == num1 + num2); }
+// if your only doing one if else see below
 
 }
+
+//
+//    public static boolean hasEqualSum(int num1,int num2, int num3){
+//
+//
+//
+//        if ((num1+num2)==num3){
+//
+//            return true;
+//
+//        }
+//
+//        else
+//
+//            return false;
+//
+//    }
--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,7 @@
 public class Main {
+
+    public static void main(String[] args) {
+        boolean isSum = EqualSumChecker.hasEqualSum(4,5,6);
+        s
+    }
 }
